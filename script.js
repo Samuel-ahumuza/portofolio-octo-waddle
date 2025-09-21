597c8c5bc17dd2af6837e33572fdd24534da268a
Samuel Ahumuza <ahumuzas515@gmail.com>
2025-09-21 12:02:19 +0300

    Initial portfolio website commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// let age = 15;
+// age = 16; 
+// const my_name ='Name';
+// my_name = 'Okello';
+// var school ='UCU';
+// let course_work_mark =25;
+// let final_exam_marks =50;
+// const myName ='name';
+// let FinalExamMark = course_work_mark+final_exam_marks
+// console.log("The Final Score is:"+FinalExamMark); 
+// console.log("When divided it equals to:" + (course_work_mark + final_exam_marks) );
+// console.log(course_work_mark*final_exam_marks);
+const userName ="John";
+const otherName = "Abaho";
+const full_name = userName + otherName;
+console.log(full_name);
+console.log("Welcome Mr."+otherName+userName
+);
